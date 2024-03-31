cb0f0405df80ba1a1cd163fa4bd269d20b12be66
Tazrian Tarfee <ttazrian759@gmail.com>
2024-03-31 18:47:21 -0400

    updated the code from lab2

--- BinaryCalculatorWebapp/src/test/java/com.ontariotechu.sofe3980U/BinaryAPIControllerTest.java
+++ BinaryCalculatorWebapp/src/test/java/com.ontariotechu.sofe3980U/BinaryAPIControllerTest.java
@@ -48,4 +48,332 @@ public class BinaryAPIControllerTest {
 			.andExpect(MockMvcResultMatchers.jsonPath("$.result").value(10001))
 			.andExpect(MockMvcResultMatchers.jsonPath("$.operator").value("add"));
     }
+
+    //the length of the first argument is greater than the second
+
+    @Test
+    public void add3() throws Exception {
+        this.mvc.perform(get("/add").param("operand1","11").param("operand2","1010"))//.andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().string("1101"));
+    }
+    @Test
+    public void addJson3() throws Exception {
+        this.mvc.perform(get("/add_json").param("operand1","11").param("operand2","1010"))//.andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.operand1").value(11))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.operand2").value(1010))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.result").value(1101))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.operator").value("add"));
+    }
+
+    //binary numbers with zero
+
+    @Test
+    public void add4() throws Exception {
+        this.mvc.perform(get("/add").param("operand1","").param("operand2","1010"))//.andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().string("1010"));
+    }
+    @Test
+    public void addJson4() throws Exception {
+        this.mvc.perform(get("/add_json").param("operand1","").param("operand2","1010"))//.andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.operand1").value(0))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.operand2").value(1010))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.result").value(1010))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.operator").value("add"));
+    }
+
+    //functions with two zeros
+
+    @Test
+    public void add5() throws Exception {
+        this.mvc.perform(get("/add").param("operand1","").param("operand2",""))//.andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().string("0"));
+    }
+    @Test
+    public void addJson5() throws Exception {
+        this.mvc.perform(get("/add_json").param("operand1","").param("operand2",""))//.andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.operand1").value(0))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.operand2").value(0))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.result").value(0))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.operator").value("add"));
+    }
+
+
+    //AND
+    //two binary numbers of the same length
+
+    @Test
+    public void and() throws Exception {
+        this.mvc.perform(get("/and").param("operand1","1000").param("operand2","1111"))//.andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().string("1000"));
+    }
+    @Test
+    public void andJson() throws Exception {
+        this.mvc.perform(get("/and_json").param("operand1","1000").param("operand2","1111"))//.andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.operand1").value(1000))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.operand2").value(1111))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.result").value(1000))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.operator").value("and"));
+    }
+
+    //the length of the first argument is less than the second
+
+    @Test
+    public void and2() throws Exception {
+        this.mvc.perform(get("/and").param("operand1","1010").param("operand2","11"))//.andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().string("10"));
+    }
+    @Test
+    public void andJson2() throws Exception {
+        this.mvc.perform(get("/and_json").param("operand1","1010").param("operand2","11"))//.andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.operand1").value(1010))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.operand2").value(11))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.result").value(10))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.operator").value("and"));
+    }
+
+    // length of the first argument is greater than the second
+
+    @Test
+    public void and3() throws Exception {
+        this.mvc.perform(get("/and").param("operand1","11").param("operand2","1010"))//.andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().string("10"));
+    }
+    @Test
+    public void andJson3() throws Exception {
+        this.mvc.perform(get("/and_json").param("operand1","11").param("operand2","1010"))//.andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.operand1").value(11))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.operand2").value(1010))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.result").value(10))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.operator").value("and"));
+    }
+
+    //and functions with a binary numbers with zero
+    @Test
+    public void and4() throws Exception {
+        this.mvc.perform(get("/and").param("operand1","").param("operand2","1010"))//.andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().string("0"));
+    }
+    @Test
+    public void andJson4() throws Exception {
+        this.mvc.perform(get("/and_json").param("operand1","").param("operand2","1010"))//.andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.operand1").value(0))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.operand2").value(1010))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.result").value(0))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.operator").value("and"));
+    }
+
+    //and functions with two zeros
+
+    @Test
+    public void and5() throws Exception {
+        this.mvc.perform(get("/and").param("operand1","").param("operand2",""))//.andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().string("0"));
+    }
+    @Test
+    public void andJson5() throws Exception {
+        this.mvc.perform(get("/and_json").param("operand1","").param("operand2",""))//.andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.operand1").value(0))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.operand2").value(0))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.result").value(0))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.operator").value("and"));
+    }
+
+
+    //OR
+    //two binary numbers of the same length
+    @Test
+    public void or() throws Exception {
+        this.mvc.perform(get("/or").param("operand1","1000").param("operand2","1111"))//.andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().string("1111"));
+    }
+    @Test
+    public void orJson() throws Exception {
+        this.mvc.perform(get("/or_json").param("operand1","1000").param("operand2","1111"))//.andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.operand1").value(1000))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.operand2").value(1111))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.result").value(1111))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.operator").value("or"));
+    }
+
+    //the length of the first argument is less than the second
+
+    @Test
+    public void or2() throws Exception {
+        this.mvc.perform(get("/or").param("operand1","1010").param("operand2","11"))//.andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().string("1011"));
+    }
+    @Test
+    public void orJson2() throws Exception {
+        this.mvc.perform(get("/or_json").param("operand1","1010").param("operand2","11"))//.andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.operand1").value(1010))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.operand2").value(11))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.result").value(1011))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.operator").value("or"));
+    }
+
+    //the length of the first argument is greater than the second
+
+    @Test
+    public void or3() throws Exception {
+        this.mvc.perform(get("/or").param("operand1","11").param("operand2","1010"))//.andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().string("1011"));
+    }
+    @Test
+    public void orJson3() throws Exception {
+        this.mvc.perform(get("/or_json").param("operand1","11").param("operand2","1010"))//.andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.operand1").value(11))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.operand2").value(1010))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.result").value(1011))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.operator").value("or"));
+    }
+
+    //or functions with a binary numbers with zero
+
+    @Test
+    public void or4() throws Exception {
+        this.mvc.perform(get("/or").param("operand1","").param("operand2","1010"))//.andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().string("1010"));
+    }
+    @Test
+    public void orJson4() throws Exception {
+        this.mvc.perform(get("/or_json").param("operand1","").param("operand2","1010"))//.andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.operand1").value(0))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.operand2").value(1010))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.result").value(1010))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.operator").value("or"));
+    }
+
+    //or functions with two zeros
+    @Test
+    public void or5() throws Exception {
+        this.mvc.perform(get("/or").param("operand1","").param("operand2",""))//.andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().string("0"));
+    }
+    @Test
+    public void orJson5() throws Exception {
+        this.mvc.perform(get("/or_json").param("operand1","").param("operand2",""))//.andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.operand1").value(0))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.operand2").value(0))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.result").value(0))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.operator").value("or"));
+    }
+
+
+    //MULTIPLY
+    //multiplication functions with two binary numbers of the same length
+
+    @Test
+    public void multiply() throws Exception {
+        this.mvc.perform(get("/multiply").param("operand1","1000").param("operand2","1111"))//.andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().string("1111000"));
+    }
+    @Test
+    public void multiplyJson() throws Exception {
+        this.mvc.perform(get("/multiply_json").param("operand1","1000").param("operand2","1111"))//.andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.operand1").value(1000))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.operand2").value(1111))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.result").value(1111000))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.operator").value("multiply"));
+    }
+
+    //the length of the first argument is less than the second
+
+    @Test
+    public void multiply2() throws Exception {
+        this.mvc.perform(get("/multiply").param("operand1","1010").param("operand2","11"))//.andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().string("11110"));
+    }
+    @Test
+    public void multiplyJson2() throws Exception {
+        this.mvc.perform(get("/multiply_json").param("operand1","1010").param("operand2","11"))//.andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.operand1").value(1010))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.operand2").value(11))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.result").value(11110))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.operator").value("multiply"));
+    }
+
+    //the length of the first argument is greater than the second
+
+    @Test
+    public void multiply3() throws Exception {
+        this.mvc.perform(get("/multiply").param("operand1","11").param("operand2","1010"))//.andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().string("11110"));
+    }
+    @Test
+    public void multiplyJson3() throws Exception {
+        this.mvc.perform(get("/multiply_json").param("operand1","11").param("operand2","1010"))//.andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.operand1").value(11))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.operand2").value(1010))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.result").value(11110))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.operator").value("multiply"));
+    }
+
+    //multiplication functions with a binary numbers with zero
+
+    @Test
+    public void multiply4() throws Exception {
+        this.mvc.perform(get("/multiply").param("operand1","").param("operand2","1010"))//.andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().string("0"));
+    }
+    @Test
+    public void multiplyJson4() throws Exception {
+        this.mvc.perform(get("/multiply_json").param("operand1","").param("operand2","1010"))//.andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.operand1").value(0))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.operand2").value(1010))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.result").value(0))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.operator").value("multiply"));
+    }
+
+    //multiplication functions with two zeros
+
+    @Test
+    public void multiply5() throws Exception {
+        this.mvc.perform(get("/multiply").param("operand1","").param("operand2",""))//.andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().string("0"));
+    }
+    @Test
+    public void multiplyJson5() throws Exception {
+        this.mvc.perform(get("/multiply_json").param("operand1","").param("operand2",""))//.andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.operand1").value(0))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.operand2").value(0))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.result").value(0))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.operator").value("multiply"));
+    }
 }
+
--- BinaryCalculatorWebapp/src/test/java/com.ontariotechu.sofe3980U/BinaryControllerTest.java
+++ BinaryCalculatorWebapp/src/test/java/com.ontariotechu.sofe3980U/BinaryControllerTest.java
@@ -57,4 +57,234 @@ public class BinaryControllerTest {
 			.andExpect(model().attribute("operand1", "111"));
     }
 
+
+    //ADD
+    //Test with two binary numbers of the same length
+    @Test
+    public void postParameterAdd() throws Exception {
+        this.mvc.perform(post("/").param("operand1","1000").param("operator","+").param("operand2","1111"))//.andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(view().name("result"))
+                .andExpect(model().attribute("result", "10111"))
+                .andExpect(model().attribute("operand1", "1000"));
+    }
+
+    // the length of the first argument is less than the second
+
+    @Test
+    public void postParameterAdd2() throws Exception {
+        this.mvc.perform(post("/").param("operand1","1010").param("operator","+").param("operand2","11"))//.andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(view().name("result"))
+                .andExpect(model().attribute("result", "1101"))
+                .andExpect(model().attribute("operand1", "1010"));
+    }
+
+
+    //the length of the first argument is greater than the second
+
+    @Test
+    public void postParameterAdd3() throws Exception {
+        this.mvc.perform(post("/").param("operand1","11").param("operator","+").param("operand2","1010"))//.andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(view().name("result"))
+                .andExpect(model().attribute("result", "1101"))
+                .andExpect(model().attribute("operand1", "11"));
+    }
+    //add functions with a binary numbers with zero
+
+    @Test
+    public void postParameterAdd4() throws Exception {
+        this.mvc.perform(post("/").param("operand1","").param("operator","+").param("operand2","1010"))//.andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(view().name("result"))
+                .andExpect(model().attribute("result", "1010"))
+                .andExpect(model().attribute("operand1", ""));
+    }
+
+    //add functions with two zeros
+
+    @Test
+    public void postParameterAdd5() throws Exception {
+        this.mvc.perform(post("/").param("operand1","").param("operator","+").param("operand2",""))//.andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(view().name("result"))
+                .andExpect(model().attribute("result", "0"))
+                .andExpect(model().attribute("operand1", ""));
+    }
+
+    //AND
+
+    //binary numbers of the same length
+
+    @Test
+    public void postParameterAnd() throws Exception {
+        this.mvc.perform(post("/").param("operand1","1000").param("operator","&").param("operand2","1111"))//.andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(view().name("result"))
+                .andExpect(model().attribute("result", "1000"))
+                .andExpect(model().attribute("operand1", "1000"));
+    }
+
+
+    //the length of the first argument is less than the second
+
+    @Test
+    public void postParameterAnd2() throws Exception {
+        this.mvc.perform(post("/").param("operand1","1010").param("operator","&").param("operand2","11"))//.andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(view().name("result"))
+                .andExpect(model().attribute("result", "10"))
+                .andExpect(model().attribute("operand1", "1010"));
+    }
+
+
+    //the length of the first argument is greater than the second
+
+    @Test
+    public void postParameterAnd3() throws Exception {
+        this.mvc.perform(post("/").param("operand1","11").param("operator","&").param("operand2","1010"))//.andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(view().name("result"))
+                .andExpect(model().attribute("result", "10"))
+                .andExpect(model().attribute("operand1", "11"));
+    }
+
+    //and functions with a binary numbers with zero
+    @Test
+    public void postParameterAnd4() throws Exception {
+        this.mvc.perform(post("/").param("operand1","").param("operator","&").param("operand2","1010"))//.andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(view().name("result"))
+                .andExpect(model().attribute("result", "0"))
+                .andExpect(model().attribute("operand1", ""));
+    }
+
+    //and functions with two zeros
+
+    @Test
+    public void postParameterAnd5() throws Exception {
+        this.mvc.perform(post("/").param("operand1","").param("operator","&").param("operand2",""))//.andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(view().name("result"))
+                .andExpect(model().attribute("result", "0"))
+                .andExpect(model().attribute("operand1", ""));
+    }
+
+    //OR
+
+    //Test with two binary numbers of the same length
+
+    @Test
+    public void postParameterOr() throws Exception {
+        this.mvc.perform(post("/").param("operand1","1000").param("operator","|").param("operand2","1111"))//.andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(view().name("result"))
+                .andExpect(model().attribute("result", "1111"))
+                .andExpect(model().attribute("operand1", "1000"));
+    }
+
+
+    // the length of the first argument is less than the second
+
+    @Test
+    public void postParameterOr2() throws Exception {
+        this.mvc.perform(post("/").param("operand1","1010").param("operator","|").param("operand2","11"))//.andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(view().name("result"))
+                .andExpect(model().attribute("result", "1011"))
+                .andExpect(model().attribute("operand1", "1010"));
+    }
+
+
+    //the length of the first argument is greater than the second
+
+    @Test
+    public void postParameterOr3() throws Exception {
+        this.mvc.perform(post("/").param("operand1","11").param("operator","|").param("operand2","1010"))//.andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(view().name("result"))
+                .andExpect(model().attribute("result", "1011"))
+                .andExpect(model().attribute("operand1", "11"));
+    }
+    //OR functions with a binary numbers with zero
+
+    @Test
+    public void postParameterOr4() throws Exception {
+        this.mvc.perform(post("/").param("operand1","").param("operator","|").param("operand2","1010"))//.andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(view().name("result"))
+                .andExpect(model().attribute("result", "1010"))
+                .andExpect(model().attribute("operand1", ""));
+    }
+
+    // OR functions with two zeros
+
+    @Test
+    public void postParameterOr5() throws Exception {
+        this.mvc.perform(post("/").param("operand1","").param("operator","|").param("operand2",""))//.andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(view().name("result"))
+                .andExpect(model().attribute("result", "0"))
+                .andExpect(model().attribute("operand1", ""));
+    }
+
+    //MULTIPLY
+
+    // two binary numbers of the same length
+
+    @Test
+    public void postParameterMultiply() throws Exception {
+        this.mvc.perform(post("/").param("operand1","1000").param("operator","*").param("operand2","1111"))//.andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(view().name("result"))
+                .andExpect(model().attribute("result", "1111000"))
+                .andExpect(model().attribute("operand1", "1000"));
+    }
+
+
+    //the length of the first argument is less than the second
+
+    @Test
+    public void postParameterMultiply2() throws Exception {
+        this.mvc.perform(post("/").param("operand1","1010").param("operator","*").param("operand2","11"))//.andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(view().name("result"))
+                .andExpect(model().attribute("result", "11110"))
+                .andExpect(model().attribute("operand1", "1010"));
+    }
+
+
+    //the length of the first argument is greater than the second
+
+    @Test
+    public void postParameterMultiply3() throws Exception {
+        this.mvc.perform(post("/").param("operand1","11").param("operator","*").param("operand2","1010"))//.andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(view().name("result"))
+                .andExpect(model().attribute("result", "11110"))
+                .andExpect(model().attribute("operand1", "11"));
+    }
+    //Multiplication functions with a binary numbers with zero
+
+    @Test
+    public void postParameterMultiply4() throws Exception {
+        this.mvc.perform(post("/").param("operand1","").param("operator","*").param("operand2","1010"))//.andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(view().name("result"))
+                .andExpect(model().attribute("result", "0"))
+                .andExpect(model().attribute("operand1", ""));
+    }
+
+    //Multiplication functions with two zeros
+
+    @Test
+    public void postParameterMultiply5() throws Exception {
+        this.mvc.perform(post("/").param("operand1","").param("operator","*").param("operand2",""))//.andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(view().name("result"))
+                .andExpect(model().attribute("result", "0"))
+                .andExpect(model().attribute("operand1", ""));
+    }
+
 }
